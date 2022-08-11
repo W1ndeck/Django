414c3824af07c3db52fe6abd0d86877b9f16ed9d
Lucas Mateus <lmateus1067@outlook.com>
2022-08-11 15:38:54 -0300

    add a primeira perg do quest, falta as opcs

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+import datetime
+from django.utils import timezone
+
 # Create your models here.
 # criando modelos dentro do banco de dados
 
@@ -8,9 +11,16 @@ class Pergunta(models.Model):  # criando a classe de pergunta
     pergunta_texto = models.CharField(max_length=200)  # tamanhao maximo
     pub_date = models.DateTimeField('data pub')  # data de publicação
 
+    def __str__(self): #este metodo(estudar POO em Python!!) serve para no shell ao inves de retornar só o ID retornar a pergunta
+        return self.pergunta_texto
+
+    def was_published_recently(self): #verifica se a pergunta foi adicionada a um dia
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Alternativa(models.Model):
     # foreigkey relaciona a pergunta com com as alternativas, e o ondelete é pra quando apagar a pergunta apagar as alternativas tambem
     pergunta = models.ForeignKey(Pergunta, on_delete=models.CASCADE)
     alternativa_text = models.CharField(max_length=200)  # tamanho da pergunta
     votes = models.IntegerField(default=0)  # contador de votos
+    def __str__(self):
+        return self.alternativa_text
